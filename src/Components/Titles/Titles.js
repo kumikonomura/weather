78fa5ca6199f1608d3db7f269841f8025f5c6449
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-11-06 20:13:23 -0800

    created title and form component

---
+++ src/Components/Titles/Titles.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Titles = _ => {
+  return (
+    <>
+      <h1>Weather Finder</h1>
+      <p>Find out temperature, conditions and more...</p>
+    </>
+  );
+};
+
+export default Titles;
