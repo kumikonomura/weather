1f5477ab288006acd544dcf950c5cdfa457b0cc5
Kumiko Nomura <kumiko.nomura@laverne.edu>
2019-11-10 20:04:43 -0800

    Updates

---
+++ precache-manifest.fc57a868b5497a358875d1ca441c3b46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe51da35e52d751ae8e63f0d5058326",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c22db5f31d5ba0cb9391",
+    "url": "/weather/static/css/main.57e6048c.chunk.css"
+  },
+  {
+    "revision": "68a8002815ccd97b9cd1",
+    "url": "/weather/static/js/2.00f4cb8d.chunk.js"
+  },
+  {
+    "revision": "c22db5f31d5ba0cb9391",
+    "url": "/weather/static/js/main.327caf7e.chunk.js"
+  },
+  {
+    "revision": "e2ccb4756aaa3ceb1a29",
+    "url": "/weather/static/js/runtime-main.02cdf23e.js"
+  },
+  {
+    "revision": "40201c61ffee379ab51d6f6e54872558",
+    "url": "/weather/static/media/weather.40201c61.jpg"
+  }
+]);
